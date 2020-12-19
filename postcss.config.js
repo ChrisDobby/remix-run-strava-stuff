b3350c9ad3bb97855277bf8ebdd51aadc252121e
chrisdobby <dobby@chrisjdobson.co.uk>
2020-12-19 17:26:26 +0000

    add postcss-import

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 const autoPrefixer = require("autoprefixer");
 const tailwindCss = require("tailwindcss");
+const postCssImport = require("postcss-import");
 
+const plugins = [tailwindCss, postCssImport];
 module.exports = {
-    plugins: process.env.NODE_ENV === "production" ? [autoPrefixer, tailwindCss] : [tailwindCss],
+    plugins: process.env.NODE_ENV === "production" ? [autoPrefixer, ...plugins] : plugins,
 };
