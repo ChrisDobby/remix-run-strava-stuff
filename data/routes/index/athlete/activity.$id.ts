798c05c277ca4c02a883e2276a9550fdfdafb59b
chrisdobby <dobby@chrisjdobson.co.uk>
2020-12-27 13:57:13 +0000

    return fetch as activity loader

--- data/routes/index/athlete/activity.$id.ts
+++ data/routes/index/athlete/activity.$id.ts
@@ -2,12 +2,6 @@ import type { Loader } from "@remix-run/data";
 import { withAuth } from "../../../auth";
 
 export const loader: Loader = withAuth(async ({ context: { stravaAuth }, params: { id } }) => {
-    try {
-        const headers = { Authorization: `Bearer ${stravaAuth.access_token}` };
-        const activityResponse = await fetch(`https://www.strava.com/api/v3/activities/${id}`, { headers });
-        const activity = await activityResponse.json();
-        return activity;
-    } catch (ex) {
-        return ex;
-    }
+    const headers = { Authorization: `Bearer ${stravaAuth.access_token}` };
+    return fetch(`https://www.strava.com/api/v3/activities/${id}`, { headers });
 });
