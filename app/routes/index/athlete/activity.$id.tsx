bd535921dd455e6ac243e03f088c561956c85305
Chris Dobson <chrisdobby.dev@gmail.com>
2021-03-07 21:21:10 +0000

    use remix cookie interface

--- app/routes/index/athlete/activity.$id.tsx
+++ app/routes/index/athlete/activity.$id.tsx
@@ -3,8 +3,7 @@ import { Activity } from "../../../athlete";
 import type { Loader } from "@remix-run/data";
 import { withAuth } from "../../../auth";
 
-export const loader: Loader = withAuth(async ({ context: { stravaAuth }, params: { id } }) => {
-    const headers = { Authorization: `Bearer ${stravaAuth.access_token}` };
+export const loader: Loader = withAuth(async ({ context: { headers }, params: { id } }) => {
     return fetch(`https://www.strava.com/api/v3/activities/${id}`, { headers });
 });
 
