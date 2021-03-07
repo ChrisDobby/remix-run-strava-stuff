b13fdfdf718f803588e3f0d75bfe1243e26169c4
Chris Dobson <chrisdobby.dev@gmail.com>
2021-03-07 08:46:58 +0000

    upgrade sessions

--- app/routes/auth.ts
+++ app/routes/auth.ts
@@ -1,8 +1,9 @@
 import { redirect } from "@remix-run/data";
 import type { Loader } from "@remix-run/data";
 import { setTokenCookie } from "../auth";
+import { getSession } from "../session";
 
-export const loader: Loader = async ({ context, session }) => {
+export const loader: Loader = async ({ context, request }) => {
     const {
         req: {
             query: { code, scope },
@@ -10,6 +11,7 @@ export const loader: Loader = async ({ context, session }) => {
         res,
     } = context;
     await setTokenCookie({ code, scope, res });
+    const session = await getSession(request.headers.get("Cookie"));
     return redirect(session.get("lastRequestPath") || "/");
 };
 
