f082e97a4c533e543ce5308b365ba73c1b3f7834
Chris Dobson <chrisdobby.dev@gmail.com>
2021-03-07 08:24:19 +0000

    Css upgrade

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,16 @@
-import { Meta, Scripts, Styles } from "@remix-run/react";
-import { Outlet, useLocation } from "react-router";
+import type { LinksFunction } from "@remix-run/react";
+import { Meta, Scripts, Links } from "@remix-run/react";
+import { useLocation } from "react-router";
+import { Outlet, useLocation } from "react-router-dom";
+import tailwind from "css:./styles/tailwind.css";
+import styles from "css:./styles/global.css";
+
+export const links: LinksFunction = () => {
+    return [
+        { rel: "stylesheet", href: tailwind },
+        { rel: "stylesheet", href: styles },
+    ];
+};
 
 export default function App() {
     const { pathname } = useLocation();
@@ -9,7 +20,7 @@ export default function App() {
             <head>
                 <meta charSet="utf-8" />
                 <Meta />
-                <Styles />
+                <Links />
             </head>
             <body>
                 <Outlet />
