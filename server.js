eb7225228bafb3274e0e8e426b0af2964dba1655
Chris Dobson <chrisdobby.dev@gmail.com>
2021-03-08 17:38:46 +0000

    added a netlify.toml file

--- server.js
+++ server.js
@@ -1,27 +1,4 @@
 require("dotenv").config();
-
-// const express = require("express");
-// const morgan = require("morgan");
-// const { createRequestHandler } = require("@remix-run/express");
-
-// const app = express();
-// app.disable("x-powered-by");
-
-// if (process.env.NODE_ENV === "development") {
-//     app.use(morgan("dev"));
-// }
-
-// app.use(express.static("public"));
-
-// app.all(
-//     "*",
-//     createRequestHandler({
-//         getLoadContext(req, res) {
-//             return { req, res };
-//         },
-//     }),
-// );
-
 const app = require("./expressApp");
 
 const port = process.env.PORT || 3000;
